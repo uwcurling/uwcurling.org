a68f1326c7893e30850aa9637c74c4a2292e9252
njdicristo <nicholasjdicr@gmail.com>
2024-10-10 18:59:30 -0500

    added springboot

---
+++ curling_backend/src/main/java/org/uwcurling/curling_backend/CurlingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.uwcurling.curling_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurlingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurlingBackendApplication.class, args);
+	}
+
+}
---
+++ curling_backend/src/test/java/org/uwcurling/curling_backend/CurlingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uwcurling.curling_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurlingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
